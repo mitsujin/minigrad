dac32b77722e6d0f4aef1d4aecfe29ca756a19ad
mitsujin <jpdobar@gmail.com>
2023-04-24 10:23:56 +0800

    more tensor tests

--- include/tensor.h
+++ include/tensor.h
@@ -1,39 +1,71 @@
 #include <concepts>
 #include <span>
+#include <memory>
+#include <algorithm>
+
 #include "tensor_shape_utils.h"
 
 namespace MiniGrad
 {
     template <typename T>
     concept Numeric = std::integral<T> || std::floating_point<T>;
+
         
     template <typename T> requires Numeric<T>
     class Tensor 
     {
     public:
+        using DataPtr = std::shared_ptr<T[]>;
+
         // More resarch
         Tensor(const std::vector<size_t>& shape)
         : m_shapeHelper(shape)
+        , m_data(new T[m_shapeHelper.size()], [](T* data) { delete[] data; })
         {
+            std::fill_n(m_data.get(), m_shapeHelper.size(), T{});
+
             // TODO: Use a memory allocator here
-            m_data = new T[m_shapeHelper.size()];
         }
 
-        ~Tensor()
+        Tensor(const std::vector<size_t>& shape, DataPtr data, int size)
+        : m_shapeHelper(shape)
+        , m_data(data)
         {
-            delete[] m_data;
+            if (m_shapeHelper.size() != size)
+            {
+                m_data = data;
+            }
+            else
+            {
+                throw std::runtime_error("Invalid shape");
+            }
+        }
+
+        Tensor reshape(const std::vector<size_t>& shape)
+        {
+            return Tensor(shape, m_data, shape.size());
         }
 
         T& at(const std::vector<size_t>& indices)
         {
             size_t index = m_shapeHelper.calculateIndex(indices);
-            return m_data[index];
+            return m_data.get()[index];
         }
 
         T at(const std::vector<size_t>& indices) const
         {
             size_t index = m_shapeHelper.calculateIndex(indices);
-            return m_data[index];
+            return m_data.get()[index];
+        }
+
+        Tensor operator = (const Tensor& other) const
+        {
+            return Tensor(m_shapeHelper.shape(), m_data, m_shapeHelper.size());
+        }
+
+        std::span<T> data()
+        {
+            return std::span<T>(m_data.get(), m_shapeHelper.size());
         }
 
         size_t size() const
@@ -87,7 +119,7 @@ namespace MiniGrad
             }
         }
 
-        T* m_data = nullptr;
         TensorShapeHelper m_shapeHelper;
+        DataPtr m_data = nullptr;
     };
 }
--- test/tensor_tests.cpp
+++ test/tensor_tests.cpp
@@ -3,10 +3,65 @@
 
 namespace MiniGrad
 {
+    namespace 
+    {
+        template <typename T>
+        void AssertCollection(std::span<T> expected, std::span<T> actual)
+        {
+            ASSERT_EQ(expected.size(), actual.size());
+            int i = 0;
+            for (auto& v : expected)
+            {
+                ASSERT_EQ(v, actual[i]);
+                i++;
+            }
+        }
+
+        template <typename T>
+        void AssertCollection(std::vector<T> expected, std::span<T> actual)
+        {
+            std::span<T> expectedS(expected.data(), expected.size());
+            AssertCollection(expectedS, actual);
+        }
+
+    }
     TEST(TensorTests, TestCreation)
     {
         Tensor<int> t({2, 3, 4});
+        t.at({0,1,0}) = 2;
         std::cout << t;
         ASSERT_EQ(24u, t.size());
+    }
+
+    TEST(TensorTests, TestModify)
+    {
+        Tensor<int> t({3});
+        t.at({1}) = 3;
+        t.at({2}) = 4;
+
+        std::vector<int>expected({0, 3, 4});
+        AssertCollection(expected, t.data());
+    }
+
+    TEST(TensorTests, TestAssignment)
+    {
+        Tensor<int> t({4});
+        t.at({0}) = 4;
+        t.at({2}) = 2;
+
+        Tensor<int> t2 = t;
+
+        AssertCollection(t.data(), t2.data());
+    }
+
+    TEST(TensorTests, TestReshape)
+    {
+        Tensor<int> t({6});
+        auto t2 = t.reshape({2, 3});
+        t2.at({1, 2}) = 5;
+
+        std::vector<int>expected({0, 0, 0, 0, 0, 5});
+        AssertCollection(expected, t.data());
+        AssertCollection(expected, t2.data());
     }
 }
