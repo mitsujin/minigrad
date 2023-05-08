c4bd5bb296eb6eda231e106a4bc5e62eb9776a53
mitsujin <jpdobar@gmail.com>
2023-05-08 15:26:57 +0800

    Using XTensor

--- build.py
+++ build.py
@@ -3,7 +3,7 @@ import subprocess
 
 def main():
     os.makedirs('build', exist_ok=True)
-    subprocess.check_call('cmake -G "Ninja" -B build -DCMAKE_CXX_STANDARD=20', shell=True)
+    subprocess.check_call('cmake -G "Ninja" -B build -DCMAKE_CXX_STANDARD=20 -DCMAKE_TOOLCHAIN_FILE=/home/jp/Documents/projects/C++/vcpkg/scripts/buildsystems/vcpkg.cmake', shell=True)
 
 if __name__ == '__main__':
     main()
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <xtensor/xarray.hpp>
+#include <xtensor/xio.hpp>
+#include <xtensor/xview.hpp>
 
 int main()
 {
+    xt::xarray<double> ar = xt::linspace<double>(0.0, 10.0, 12.0);
+    ar.reshape({4,3});
+    std::cout << ar << std::endl;
+    
     std::cout << "Hello world\n";
 }
